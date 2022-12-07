dafe6f4a4365c78b9e4b779413d63afb721c9434
Altant <28458922+Altant457@users.noreply.github.com>
2022-12-07 14:21:40 +0100

    Edit vite config

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,23 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: `./`,
-  build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: `assets/[name].js`,
-        chunkFileNames: `assets/[name].js`,
-        assetFileNames: `assets/[name].[ext]`
+export default defineConfig(({ command, mode }) => {
+  if (command === "build") {
+    plugins: [react()],
+    base: `./`,
+    build: {
+      rollupOptions: {
+        output: {
+          entryFileNames: `assets/[name].js`,
+          chunkFileNames: `assets/[name].js`,
+          assetFileNames: `assets/[name].[ext]`
+        }
       }
+    }
+  } else {
+    return {
+      plugins: [react()],
+      base: `/exam/`
     }
   }
 })
